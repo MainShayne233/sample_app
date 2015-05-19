20a24598eb639da4121018a9a692b3d83f669c8e
Shayne <shaynetremblay@hotmail.com>
2015-05-19 16:50:38 -0400

    add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def tempgreeting
+  	render text: "This is temporary"
+  end
 end
